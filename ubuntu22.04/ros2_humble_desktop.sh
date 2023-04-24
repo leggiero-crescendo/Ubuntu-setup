15fe3c452f5af543d3ff3efcf8a98cd28cd72987
leggiero-crescendo <125112464+leggiero-crescendo@users.noreply.github.com>
2023-04-24 23:33:31 +0900

    Update ros2_humble_desktop.sh

--- ubuntu22.04/ros2_humble_desktop.sh
+++ ubuntu22.04/ros2_humble_desktop.sh
@@ -22,7 +22,8 @@ export LANG=en_US.UTF-8
 
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/nullsudo apt install -y ros-humble-desktop
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null 
+sudo apt install -y ros-humble-desktop
 source /opt/ros/humble/setup.bash
 
 sudo apt update && sudo apt install -y \
